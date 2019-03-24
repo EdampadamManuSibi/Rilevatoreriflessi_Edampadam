de08841f04701faad9cc4a58718f1a7c1540f4ca
Edampadam Manu SIbi <manusibyedampadam@yahoo.it>
2019-03-24 07:55:48 +0100

    Terzo Edampadam

--- Rilevatoreriflesssi/Rilevatoreriflesssi.ino
+++ Rilevatoreriflesssi/Rilevatoreriflesssi.ino
@@ -1,50 +1,52 @@
 #include <LiquidCrystal.h>
-
 LiquidCrystal (12,10,8,6,4,2)
 
 int suonobuzzer = 1;
-int bottonereset = 3;
+int bottone-reset = 3;
 int bottone-led = 5;
 int bottone-suono = 7;
 int rgbled = (9,11);
-
+int ledbianco = (13);
 
 void setup() {
   // put your setup code here, to run once:
-
-Comandobottoneled;   
-Comandobottonebuzer;
-Displaylcd;
+pinMode (ledbianco, OUTPUT);
+pinMode (suonobuzzer, OUTPUT);
+pinMode (rgbled, OUTPUT);
+pinMode (bottone-reset, INPUT);
+pinMode (bottone-led, INPUT);
+pinMode (bottone-suono, INPUT);
+lcd.begin (16,2);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+Comandobottoneled;   
+Comandobottonebuzer;
+Displaylcd;
+Buzzer-rgb;
 }
 
 void Comandobottoneled
 {
-buttonstatus=digitalRead(button);
-if (button status == LOW);
-{
-digitalWrite (ledacaso, HIGH);
-delay(5000);
-digitalWrite (ledacaso, HIGH);
-}
-else
-{
-digitalWrite (ledacaso, HIGH);
-}
+while (ledbianco = HIGH)
+digitalWrite (bottone-led = HIGH)
+delay (random(1000,5000))
 }
 
 void Comandobottonebuzzer
 {
-if (suonobuzzer == HIGH);
-{
- digitalWrite (rgbled = HIGH);
- tone (suonobuzzer, 1000, 2000)
+while (suonobuzzer = HIGH)
+digitalWrite (bottone-suono = HIGH)
+delay (random(1000,5000))
 }
+
+void Buzzer-rgb
+{
+while (suonobuzzer = HIGH)
+digitalWrite (rgbled = HIGH)
+delay (random(1000,5000))
 }
 
 void Displaylcd
