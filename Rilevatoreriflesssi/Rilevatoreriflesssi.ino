a2d2d91a57c7376706b7d8246efb37f6673f2fe7
Edampadam Manu SIbi <manusibyedampadam@yahoo.it>
2019-03-27 20:49:24 +0100

    Settimo Edampadam

--- Rilevatoreriflesssi/Rilevatoreriflesssi.ino
+++ Rilevatoreriflesssi/Rilevatoreriflesssi.ino
@@ -1,13 +1,13 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(8,9,10,11,12,13);
 
-int suonobuzzer = 4;
-int bottone_reset = 7;
+int suonobuzzer = 2;
+int bottone_reset = 4;
 int bottone_led = 6;
-int bottone_suono = 5;
-int rgbledrosso = 2;
-int rgbledverde = 1;
-int ledgiallo = 3;
+int bottone_suono = 7;
+int rgbledrosso = 3;
+int rgbledverde = 5;
+int ledgiallo = 1;
 int temposuonobuzzer;
 int tempoled;
 
@@ -28,13 +28,15 @@ void loop() {
   // put your main code here, to run repeatedly:
 if (digitalRead(bottone_reset)==HIGH)
 {
-Comandobottoneled();   
+lcd.print("Test start");
+delay(2000);
+Displaylcd();
+Comandobottoneled(); 
+Displaylcd2();  
 Comandobottonebuzzer();
 Buzzer_rgb();
 Led_rgb();
-Displaylcd();  
 }
-
 
 }
 
@@ -54,7 +56,7 @@ lcd.print(tempoled);
 void Comandobottonebuzzer()
 {
 delay (random(1000,5000));
-tone(suonobuzzer,1000);
+tone(suonobuzzer,500);
 double temposuonobuzzer = 0;
 while (digitalRead (bottone_suono) ==LOW)
 {
@@ -67,11 +69,11 @@ noTone(suonobuzzer);
 
 void Buzzer_rgb()
 {
-if (temposuonobuzzer < 200)
+if (temposuonobuzzer < 300)
 {
 digitalWrite (rgbledverde, HIGH);
 }
-else if (temposuonobuzzer > 200)
+else if (temposuonobuzzer > 300)
 {
 digitalWrite (rgbledrosso, HIGH);
 }
@@ -79,11 +81,11 @@ digitalWrite (rgbledrosso, HIGH);
 
 void Led_rgb()
 {
-if (tempoled < 200)
+if (tempoled < 300)
 {
 digitalWrite (rgbledverde, HIGH);
 }
-else if (tempoled > 200)
+else if (tempoled > 300)
 {
 digitalWrite (rgbledrosso, HIGH);
 }
@@ -95,6 +97,9 @@ lcd.setCursor(0, 0);
 lcd.print("Test con luce");
 delay (2000);
 lcd.clear();
+}
+void Displaylcd2()
+{
 lcd.setCursor(0, 1);
 lcd.print("Test con suono");
 delay (2000);
